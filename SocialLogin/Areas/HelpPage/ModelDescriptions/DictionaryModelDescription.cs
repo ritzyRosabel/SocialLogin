753a53f2d14fb4c2c1dd85dd5546ea22eb2c62ce
Ritzy-Rosabel <mipoolugbenga@gmail.com>
2020-03-03 10:56:19 +0100

    Add project files.

---
+++ SocialLogin/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SocialLogin.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ SocialLogin/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SocialLogin.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
